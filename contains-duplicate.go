ba7dbcd141f0b49c562ba1b7685131da123e76fd
ShamimEnamul <shamim.enamul23@gmail.com>
2022-10-12 06:37:43 +0600

    contains-duplicate

---
+++ contains-duplicate.go
@@ -0,0 +1,14 @@
+func containsDuplicate(nums []int) bool {
+
+	for i := 0; i < len(nums); i++ {
+
+		for j := 0; j < len(nums); j++ {
+
+			if nums[i] == nums[j] && i != j {
+				return true
+			}
+		}
+	}
+
+	return false
+}
